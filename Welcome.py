058f1f14296eaeae7be1fb7abeb99b38c39946c7
RuYi Chiu <t90079@tsust.edu.tw>
2025-03-02 11:22:52 +0800

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -1,1 +1,10 @@
-def print
+def print_welcome():
+    name = input("請輸入你的名字：")
+    print(f"""
+    ╔═══════════════════════════╗
+    ║  歡迎來到 {name} 的GitHub！  ║
+    ╚═══════════════════════════╝
+    """)
+
+if __name__ == "__main__":
+    print_welcome()
